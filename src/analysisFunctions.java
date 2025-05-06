798934c27980b4cf242f2984a13e93a45af43e00
jf797 <jf797@student.aru.ac.uk>
2025-05-06 17:21:28 +0100

    Initial add.
    - Created basic blueprint of files used for application.

---
+++ src/analysisFunctions.java
@@ -0,0 +1,21 @@
+public class analysisFunctions {
+    
+}
+
+
+
+class displayAll {
+    String name;
+    String description;
+
+
+    displayAll(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    void actionFunction() {
+
+    }
+
+}
---
+++ src/application.java
@@ -0,0 +1,50 @@
+public class application {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Define regex labels
+
+        regex archRegex = new regex("ArchRegex", "Regex used for ArchLinux journalctl", "^(?P<timestamp>[a-zA-Z]+\\s\\d+\\s\\d+:\\d+:\\d+)\\s(?P<hostname>[^\\s]+)\\s(?P<facility>[^\\:]+):\\s(?P<message>.*)");
+
+        archRegex.displayRegexDefinition();
+
+        // Define analysis functions
+
+    }
+}
+class collectLog {
+
+}
+class internalLog {
+
+}
+
+class logFile {
+    String name;
+    String filePath;
+
+    logFile(String name, String filePath) { 
+        this.name = name;
+        this.filePath = filePath;
+    }
+
+    
+}
+
+class regex {
+    String name;
+    String description;
+    String regexCode;
+
+    regex(String name, String description, String regexCode) {
+        this.name = name;
+        this.description = description;
+        this.regexCode = regexCode;
+    }
+
+    void displayRegexDefinition() {
+        System.out.println("\nName: " + name + "\nDescription: " + description + "\nRegex Code: " + regexCode);
+    } 
+}
+
+
